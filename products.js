f0934bfbd00bb980a18f2caf50a0f44cf7d8a150
Szafenzo007 <113356941+Szafenzo007@users.noreply.github.com>
2023-09-07 14:47:58 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        id: "1",
+        category: "Long Sleeve",
+        name: "Long Sleeves",
+        price: " 29,99 $ ",
+        images: ["/img/products/longsleevesnobg.png"]
+    },
+    {
+        id: "2",
+        category: "Logo",
+        name: "Logo",
+        price: " 100 £",
+        images: ["/img/products/PuffoLogo.png"]
+    },
+    {
+        id: "3",
+        category: "Shorts",
+        name: "Black Shorts",
+        price: " 19.99 £",
+        images: ["/img/products/shortsblacknoBg.png"]
+    },
+    {
+        id: "4",
+        category: "White Shorts",
+        name: "Sony FX3",
+        price: " 24.99 $",
+        images: ["/img/products/shortswhite.png"]
+    },
+    {
+        id: "5",
+        category: "no sleeves",
+        name: "Trunk",
+        price: "29.99 $",
+        images: ["/img/products/trunkblacknoBg.png"]
+    },
+    {
+        id: "6",
+        category: "T-Shirt",
+        name: "T-Shirt",
+        price: " 25.99 $",
+        images: ["/img/products/tshirtfrontNoBg.png"]
+    },
+]
+
+export default products
